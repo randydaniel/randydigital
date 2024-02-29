41295812b26fbab15ef551437b3ecc7fe49f0517
@randydaniel <romeomikedelta@gmail.com>
2024-02-29 10:36:21 -0500

    Updates to Drawer.svelte.

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -10,14 +10,16 @@
   }
 </script>
 
-<div class="h-screen border-t border-grid-lines opacity-90 z-10 drawer {workItem ? 'open' : ''}">
+<div class="h-screen border-t border-grid-lines z-10 drawer {workItem ? 'open' : ''}">
   <div class="container">
     <div class="grid grid-cols-1">
       {#if workItem}
         <!-- Display the work item's details here -->
         <div class="content max-h-screen p-8 border-x border-grid-lines">
           <button class="close-btn" on:click={closeDrawer}>⊗</button>
-          <h2 class="text-8xl font-bold uppercase text-center">{workItem.title}</h2>
+          <div class="sticky top-0">
+            <h2 class="text-8xl font-bold uppercase text-center">{workItem.title}</h2>
+          </div>
           <div class="grid grid-rows-1 max-h-full">
             <div class="flex items-top justify-center p-8">
               <img class="max-h-100 w-auto" src={workItem.img} alt={workItem.title} />
@@ -37,9 +39,17 @@
   transform: translateY(100%); /* Start hidden */
   transition: transform 0.3s ease-in-out;
   background: var(--background-color); /* Add a background to make the content visible */
+  overflow-y: hidden; /* Prevent scrolling on the drawer itself */
 
   &.open {
     transform: translateY(0); /* Slide in */
+  }
+
+  .content {
+    max-height: 100vh; /* Adjust based on your needs */
+    overflow-y: auto; /* Enable vertical scrolling */
+    padding: 2rem;
+    box-sizing: border-box; /* Ensures padding is included in the height calculation */
   }
 
   .close-btn {
@@ -50,6 +60,7 @@
     border: none;
     cursor: pointer;
     font-size: 2rem; // Adjust as needed
+    font-weight: lighter;
   }
 }
 </style>
