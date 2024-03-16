2efbd4f83e12fd181883498cd3956c12c6551924
@randydaniel <romeomikedelta@gmail.com>
2024-03-16 12:42:06 -0400

    Reponsive Services.svelte.

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -7,28 +7,28 @@
 
 <section id="services" class="border-b border-grid-lines">
   <div class="container">
-    <div class="grid grid-cols-3 border-x border-grid-lines">
-      <div class="p-8 border-r border-grid-lines max-h-[713px] overflow-hidden">
+    <div class="grid grid-auto-rows lg:grid-cols-3 border-x border-grid-lines">
+      <div class="p-8 border-b lg:border-r lg:border-b:0 border-grid-lines max-h-[713px] overflow-hidden">
         <h2 class="text-2xl mb-4">Design System Development</h2>
         <p class="font-light tracking-tight">Crafting cohesive, scalable design solutions that ensure brand consistency and enhance user experience across digital platforms.</p>
-        <img class="mt-8" src={DesignSystem} alt="Design System Development" />
+        <img class="m-auto mt-8" src={DesignSystem} alt="Design System Development" />
       </div>
-      <div class="grid grid-rows-2 border-r border-grid-lines max-h-[713px]">
+      <div class="grid grid-rows-2 border-r-0 lg:border-r border-grid-lines max-h-[713px]">
         <div class="p-8 border-b border-grid-lines">
           <h2 class="text-2xl mb-4">User Interface Design</h2>
           <p class="font-light tracking-tight">Intuitive, visually appealing interfaces that improve user experience and drive engagement.</p>
-          <img class="mt-8" src={UI} alt="User Interface Design" />
+          <img class="m-auto mt-8" src={UI} alt="User Interface Design" />
         </div>
-        <div class="p-8 overflow-hidden">
+        <div class="p-8 overflow-hidden border-grid-lines border-b lg:border-b-0">
           <h2 class="text-2xl mb-4">Human-Computer Interaction</h2>
           <p class="font-light tracking-tight">Focusing on designing seamless, user-centered experiences that bridge the gap between people and digital technology.</p>
-          <img class="mt-8" src={HCI} alt="Human-Computer Interaction" />
+          <img class="m-auto mt-8" src={HCI} alt="Human-Computer Interaction" />
         </div>
       </div>
       <div class="p-8 max-h-[713px]">
         <h2 class="text-2xl mb-4">Web Development</h2>
         <p class="font-light tracking-tight">Delivering responsive, high-performance websites for compelling user experiences and digital innovation.</p>
-        <img class="mt-8" src={WebDev} alt="Web Development" />
+        <img class="m-auto mt-8" src={WebDev} alt="Web Development" />
       </div>
     </div>
   </div>
